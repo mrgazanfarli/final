7b0491985e86f2536e04c1fea0b7af4192894691
mrgazanfarli <mr.gazanfarli@gmail.com>
2019-10-31 18:06:10 +0400

    active link identifiers added

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import TodoCreator from './TodoCreator';
 import TodoList from './TodoList';
 import PostList from './PostList';
 import PostSearch from './PostSearch';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
     render() {
@@ -14,8 +14,8 @@ class App extends React.Component {
                 <div className="container">
                     <div className="row pt-4">
                         <div className="col-12">
-                            <Link to="/todos" className="btn btn-primary">Todo List</Link>
-                            <Link to="/posts" className="btn btn-primary ml-3">Posts</Link>
+                            <NavLink to="/todos" activeClassName="btn-primary text-white" className="btn btn-outline-primary">Todo List</NavLink >
+                            <NavLink to="/posts" activeClassName="btn-primary text-white" className="btn btn-outline-primary ml-3">Posts</NavLink >
                         </div>
                         <Switch>
                             <Route path="/todos">
